ef441d69874a1a60dd81ca97cdd65a7d3e81eb18
michiwo <mk1104kado@gmail.com>
2023-09-11 08:29:37 -0700

    Add section 2 ep 24

--- demo/controllers/notes.php
+++ demo/controllers/notes.php
@@ -3,9 +3,9 @@
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$heading = 'Notes';
+$heading = 'My Notes';
 
-$note = $db->query('select * from notes where user_id = :1', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where user_id = :1', ['id' => $_GET['id']])->get();
 
 dd($db);
 
