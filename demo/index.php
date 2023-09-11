aeae651fd8bcf4bcf97e239e479c4ef16d8ae7f6
michiwo <mk1104kado@gmail.com>
2023-09-10 17:07:03 -0700

    Add ep 8

--- demo/index.php
+++ demo/index.php
@@ -9,25 +9,47 @@
     $books = [
       [
         'name' => 'Do Androids Dream of Electric Sheep',
-        'author' => 'The Langoliers',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
         'purchaseUrl' => 'http://example.com'
       ],
       [
         'name' => 'Project Hail Mary',
         'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+      ],
+      [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
         'purchaseUrl' => 'http://example.com'
       ]
     ];
   ?>
 
+  <?php 
+    function filterByAuthor($books, $author) {
+      $filterBooks = [];
+
+      foreach ($books as $book) {
+        if ($book['author'] === $author) {
+          $filterBooks = $book;
+        }
+      }
+
+      return $filterBooks;
+    }
+  ?>
+
   <ul>
-    <?php foreach ($books as $book) : ?>
+    <?php foreach (filterByAuthor($books, 'Andy Weir') as $book) : ?>
       <li>
         <a href="<?= $book['purchaseUrl'] ?>">
-          <?= $book['name']; ?>
+          <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
         </a>
       </li>
-    <?php endforeach ?>
+    <?php endforeach; ?>
   </ul>
 
 </body>
