88a352a3e83bbde4e9b2f6b76540836924b88e7c
michiwo <mk1104kado@gmail.com>
2023-09-11 05:37:58 -0700

    Add section 2 ep 19

--- demo/Database.php
+++ demo/Database.php
@@ -3,14 +3,17 @@
 class Database {
   public $connection;
 
-  public function __connection() {
-    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+  public function __connection($config, $username = 'root', $password = '') {
+    $dsn = 'myaql:' . http_build_query($config, '', ';');
 
-    $this->connection = new PDO($dsn);
+    $this->connection = new PDO($dsn, $username, $password, [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
   }
 
   public function query($query){
     $statement = $this->connection->prepare($query);
+    
     $statement->execute();
 
     return $statement;
