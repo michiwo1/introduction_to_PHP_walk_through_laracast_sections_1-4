7e97ec2c0e95ee0586ced44c247f5ecc77cb4019
michiwo <mk1104kado@gmail.com>
2023-09-10 17:59:31 -0700

    Add section 1 ep 10

--- demo/index.php
+++ demo/index.php
@@ -1,40 +1,17 @@
-<?php
+<?php 
 
-$books = [
-  [
-    'name' => 'Do Androids Dream of Electric Sheep',
-    'author' => 'Philip K. Dick',
-    'releaseYear' => 1968,
-    'purchaseUrl' => 'http://example.com'
-  ],
-  [
-    'name' => 'Project Hail Mary',
-    'author' => 'Andy Weir',
-    'releaseYear' => 2021,
-    'purchaseUrl' => 'http://example.com'
-  ],
-  [
-    'name' => 'The Martian',
-    'author' => 'Andy Weir',
-    'releaseYear' => 2011,
-    'purchaseUrl' => 'http://example.com'
-  ]
+$business = [
+  'name' => 'Laracasts',
+  'cost' => 15,
+  'categories' => [ "Testing", "PHP", "JavaScript"]
 ];
 
-function filter($items, $fn) {
-  $filteredItems = [];
-
-  foreach ($items as $item) {
-    if ($fn($item)) {
-      $filterBooks[] = $item;
-    }
-  }
-
-  return $filteredItems;
+function register() {
+  // Create the user record in the db.
+  // Log them in.
+  // Send a welcome email.
+  // Redirect to their new dashboard
 }
 
-$filteredBooks = array_filter($books, function ($book) {
-  return $book['author'] === 'Andy Weir';
-});
 
 require "index.view.php";
