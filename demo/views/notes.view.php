cad12d35eeec342c6ce9aaf053b1253a6e5c11d0
michiwo <mk1104kado@gmail.com>
2023-09-11 09:04:58 -0700

    Add section 2 ep 25

--- demo/views/notes.view.php
+++ demo/views/notes.view.php
@@ -4,6 +4,7 @@
 
 <main>
   <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+  <ul>
     <?php foreach ($notes as $note) : ?>
       <li>
         <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
@@ -11,6 +12,11 @@
         </a>
       </li>
     <?php endforeach; ?>
+  </ul>
+
+  <p>
+    <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
+  </p>
   </div>
 </main>
 
