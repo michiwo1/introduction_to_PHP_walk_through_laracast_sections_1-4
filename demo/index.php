32e0db540d817a4349a79a9160a6a33de27c2fb0
michiwo <mk1104kado@gmail.com>
2023-09-11 05:50:29 -0700

    Add section 2 ep 20

--- demo/index.php
+++ demo/index.php
@@ -5,9 +5,11 @@ require 'functions.php';
 require 'Database.php';
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts") -> fetchAll();
+$id = $_GET['id'];
+$query = "select * from posts where id = id";
+
+$posts = $db->query($query, [':id' => $id]) -> fetchAll();
 
 dd($posts);
