269a3d18ab46581f32ec085aa50df85ed64d0534
michiwo <mk1104kado@gmail.com>
2023-09-10 18:44:46 -0700

    Add section 2 ep 12

--- demo/index.php
+++ demo/index.php
@@ -1,17 +1,4 @@
 <?php 
 
-$business = [
-  'name' => 'Laracasts',
-  'cost' => 15,
-  'categories' => [ "Testing", "PHP", "JavaScript"]
-];
-
-function register() {
-  // Create the user record in the db.
-  // Log them in.
-  // Send a welcome email.
-  // Redirect to their new dashboard
-}
-
 
 require "index.view.php";
