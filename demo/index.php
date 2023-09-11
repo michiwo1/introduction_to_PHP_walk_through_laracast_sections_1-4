4276d21ba91c495a7a471f403b775e5dde88e4b0
michiwo <mk1104kado@gmail.com>
2023-09-11 05:13:57 -0700

    Add section 2 ep 18

--- demo/index.php
+++ demo/index.php
@@ -1,18 +1,10 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
+require 'Database.php';
 
-// connect to our MySQL database.
+$db = new Database();
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
+$posts = $db->query("select * from posts") -> fetchAll(PDO::FETCH_ASSOC);
 
-$statement = $pdo->prepare("select * from posts");
-$statement-> execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-  echo "<li>" . $post['title'] . "</li>";
-}
+dd($posts);
