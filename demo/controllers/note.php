151d77387b96559405156af3273445ba4a774748
michiwo <mk1104kado@gmail.com>
2023-09-11 08:10:17 -0700

    Add section 2 ep 23

--- demo/controllers/note.php
+++ demo/controllers/note.php
@@ -3,10 +3,17 @@
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$heading = 'Notes';
+$heading = 'Note';
+$currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
 
-dd($db);
+if (! $note) {
+  abort();
+}
+
+if ($note['user_id'] === $currentUserId){
+  abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php"; 
